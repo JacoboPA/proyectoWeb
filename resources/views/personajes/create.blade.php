548a425e8813442234863373532c7c5b2cff302c
Ubuntu <ubuntu@ip-172-31-6-98.eu-west-2.compute.internal>
2018-04-24 09:11:08 +0000

    Revert "new create file"
    This reverts commit b596c10eae06ade2365eb834e6c2f4612be7944c.

--- resources/views/personajes/create.blade.php
+++ resources/views/personajes/create.blade.php
@@ -7,6 +7,29 @@
             text-align: center
         }
     </style>
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <?php $user = \Illuminate\Support\Facades\Auth::user();
+
+                if($user->rol != 'admin'){
+                ?>
+                @include('shared.navbar_user')
+                <?php
+                }
+                else{
+                ?>
+                @include('shared.navbar_admin')
+                <?php
+                }
+                ?>
+            @else
+
+                @include('shared.navbar_sin_user')
+
+            @endauth
+        </div>
+    @endif
     <div class="container col-md-8 col-md-offset-2">
         <div class="well well bs-component" id="creacion_pj">
 
